c9303148c380632bb80233ffcce32fdfc6fca60b
Mundia1 <waithaka.mundia@gmail.com>
2025-04-21 11:33:36 +0300

    Implement add to cart functionality in Item component

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  // State for tracking whether the item is in the cart
+  const [inCart, setInCart] = useState(false);
+
+  // Toggle the item in and out of the cart
+  const toggleCart = () => setInCart(!inCart);
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={toggleCart} className="add">
+        {inCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
